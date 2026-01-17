f17e9956f241836cca55bcb7cbd428e67d1414f5
우태호 <{>
2026-01-17 15:16:33 +0900

    Quiz11382 Solved

--- solve.ac/5tier/ADivedeB.java
+++ solve.ac/5tier/ADivedeB.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class ADivedeB {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        double a = Double.parseDouble(st.nextToken());
-        double b = Double.parseDouble(st.nextToken());
+        String[] input = br.readLine().trim().split("\\s+");
+        double a = Double.parseDouble(input[0]);
+        double b = Double.parseDouble(input[1]);
 
         System.out.println(a / b);
     }
--- solve.ac/5tier/BasicOperations.java
+++ solve.ac/5tier/BasicOperations.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
 
 public class BasicOperations {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().trim().split("\\s+");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
 
         System.out.println(a + b);
         System.out.println(a - b);
