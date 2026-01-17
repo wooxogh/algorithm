f8d582a5c1af1d317bf1f2fec0dd7b814ecc74d1
우태호 <{>
2026-01-17 18:26:08 +0900

    2026/01/17 Solved

---
+++ solve.ac/5tier/array/Assignment.java
@@ -0,0 +1,8 @@
+//Quiz 5597
+package array;
+
+public class Assignment {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ solve.ac/5tier/loop/MultiplationTable.java
@@ -0,0 +1,23 @@
+//Quiz 2739
+package loop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MultiplationTable {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        for (int i = 1; i < 10; i++) {
+            bw.write(a + " * " + i + " = " + a * i);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
