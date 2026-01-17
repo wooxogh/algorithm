ec12cd383ad0fb8427d13a899be66bbd8c393709
우태호 <{>
2026-01-17 19:17:39 +0900

    2026/01/17 Solved3

--- solve.ac/5tier/conditions/Exam.java
+++ solve.ac/5tier/conditions/Exam.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public interface Exam {
+public class Exam {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
--- solve.ac/5tier/loop/MultiplecationTable.java
+++ solve.ac/5tier/loop/MultiplecationTable.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class MultiplationTable {
+public class MultiplecationTable {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
