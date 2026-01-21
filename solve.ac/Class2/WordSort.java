c792cf33de892f3018d9f82c8802c99dc9e6cd37
우태호 <{>
2026-01-21 17:54:52 +0900

    2026/01/21 Solved

--- solve.ac/Class2/WordSort.java
+++ solve.ac/Class2/WordSort.java
@@ -1,12 +1,38 @@
+//Quiz 1181
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WordSort {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> list = new ArrayList<>(set);
+
+        Collections.sort(list, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            }
+            return a.compareTo(b);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s).append('\n');
+        }
+
+        System.out.print(sb);
+
     }
 }
