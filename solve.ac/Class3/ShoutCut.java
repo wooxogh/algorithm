c2e4029dc1a4ed4e02f00060362952d388ab7923
우태호 <{>
2026-02-08 23:33:24 +0900

    2026/02/08 Solved

--- solve.ac/Class3/ShoutCut.java
+++ solve.ac/Class3/ShoutCut.java
@@ -2,29 +2,54 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class ShoutCut {
-    static int target_x;
-    static int target_y;
-    static int[][] map;
-    static int[][] result;
+    static int n, m, target_x, target_y;
+    static int[][] map, result;
+    static boolean[][] visited;
+    
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
 
-    static void dfs(int x, int y) {
-        
+    static void bfs(int sx, int sy) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sx, sy});
+        visited[sx][sy] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int d=0; d<4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nx<0 || ny<0 || nx>=n || ny>=m) continue;
+                if (visited[nx][ny]) continue;
+                if (map[nx][ny]==0) continue;
+
+                visited[nx][ny] = true;
+                result[nx][ny] = result[x][y] + 1;
+                q.offer(new int[]{nx, ny});
+            }
+        }
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        map = new int[x][y];
-        result = new int[x][y];
-        for (int i = 0; i < x; i++) {
+        map = new int[n][m];
+        result = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < y; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
 
                 if (map[i][j] == 2) {
@@ -34,6 +59,19 @@ public class ShoutCut {
             }
         }
 
-        dfs(target_x, target_y);
+        bfs(target_x, target_y);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (map[i][j]==0) sb.append(0).append(" ");
+                else if (!visited[i][j]) sb.append(-1).append(" ");
+                else sb.append(result[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);        
     }
 }
