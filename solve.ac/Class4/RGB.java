55df44f477f9bfea72d8e18fede852ec146cdf43
우태호 <{>
2026-02-26 21:35:23 +0900

    2026/02/26 Solved

--- solve.ac/Class4/RGB.java
+++ solve.ac/Class4/RGB.java
@@ -9,9 +9,29 @@ public class RGB {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
+        int[][] cost = new int[T][3];
+        int[][] dp = new int[T][3];
+
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
         }
+
+        // 첫 집 초기화
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        for (int i = 1; i < T; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        }
+
+        int answer = Math.min(dp[T - 1][0], Math.min(dp[T - 1][1], dp[T - 1][2]));
+
+        System.out.println(answer);
     }
 }
--- solve.ac/Class4/Triangle.java
+++ solve.ac/Class4/Triangle.java
@@ -2,10 +2,45 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Triangle {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] cost = new int[T][T];
+        int[][] dp = new int[T][T];
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = cost[0][0];
+
+        for (int i = 1; i < T; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + cost[i][j];
+                } 
+                else if (j == i) {
+                    dp[i][j] = dp[i-1][j-1] + cost[i][j];
+                } 
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + cost[i][j];
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < T; i++) {
+            answer = Math.max(answer, dp[T-1][i]);
+        }
+
+        System.out.println(answer);
+
     }
 }
