46aa6683dab7ebf1d6cd3ef168515abfece6b35c
우태호 <{>
2026-03-24 20:14:42 +0900

    2026/03/24 Solved

--- solve.ac/Class4/Backpack.java
+++ solve.ac/Class4/Backpack.java
@@ -11,6 +11,22 @@ public class Backpack {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        
+        int[] weight = new int[N];
+        int[] value = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            for (int w = K; w >= weight[i]; w--) {
+                dp[w] = Math.max(dp[w], dp[w - weight[i]] + value[i]);
+            }
+        }
+
+        System.out.println(dp[K]);
+
     }
 }
---
+++ solve.ac/Class4/HideAndSeek3.java
@@ -0,0 +1,17 @@
+//Quiz 13549
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HideAndSeek3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
