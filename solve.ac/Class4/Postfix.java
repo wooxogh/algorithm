6c36a3fc66438ea9a58d84726cb265261f803233
우태호 <{>
2026-04-04 22:00:14 +0900

    2026/04/04 Solved

---
+++ solve.ac/Class4/Postfix.java
@@ -0,0 +1,11 @@
+//Quiz 1918
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Postfix {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //TODO
+    }
+}
